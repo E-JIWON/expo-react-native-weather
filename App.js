3421abc4312d94898570a15d1ecc1c0588c36634
jiwon <meowoof01@gmail.com>
2024-08-26 00:02:17 +0900

    #2.6 scrollView, Dimensions

--- App.js
+++ App.js
@@ -1,26 +1,73 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+import React from "react";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>HHHHHEEEEELLLOO~</Text>
-      {/* 이 status-bar는 앱의 배터리, 시간, 와이파이 등 을 나타냄 */}
       <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      {/* <View style={styles.weather}> */}
+      {/* scrollbiew는 화면보다 커야해서 flex:3을 주면 작동 안됨 */}
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
+      {/* </View> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // 아래 continaer, text는 네이밍 규칙 없음
-  container: {
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
+  container: { flex: 1, backgroundColor: "tomato" },
+  city: {
+    flex: 1.2,
     justifyContent: "center",
+    alignItems: "center",
   },
-  text: {
-    fontSize: 28,
-    color: "black",
+  cityName: {
+    color: "white",
+    fontSize: 58,
+    fontWeight: "500",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 168,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
