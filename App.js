390adda23b912f42294e281e009844b5dcb89864
jiwon <meowoof01@gmail.com>
2024-08-25 22:35:03 +0900

    first commit

--- App.js
+++ App.js
@@ -4,22 +4,23 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello I made a RN App!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>HHHHHEEEEELLLOO~</Text>
+      {/* 이 status-bar는 앱의 배터리, 시간, 와이파이 등 을 나타냄 */}
+      <StatusBar style="light" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // 이 이름은 정해진 규칙이 없음
+  // 아래 continaer, text는 네이밍 규칙 없음
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
     fontSize: 28,
-    color: "red",
+    color: "black",
   },
 });
